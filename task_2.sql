fdc0fa559732fd3fb69541d5dfa8fdd4c3164613
Mohamed Lazaar <lazzarmohamed10@gmail.com>
2025-12-14 13:54:13 +0100

    Create Magical Tables

---
+++ task_2.sql
@@ -0,0 +1,41 @@
+-- create database alx_book_store
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+-- use database alx_book_store
+USE alx_book_store;
+-- create table authors
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+-- create table books
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+-- create table customers
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT 
+);
+-- create table orders
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
+-- create table order_details
+CREATE TABLE IF NOT EXISTS Order_Details{
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+}
