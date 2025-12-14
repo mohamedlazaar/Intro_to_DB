1de0b65a083f07c1774429abc2118375d710057a
Mohamed Lazaar <lazzarmohamed10@gmail.com>
2025-12-14 13:17:40 +0100

    build database

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,22 +9,16 @@ def create_database():
             user="root",
             password="password"
         )
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
 
-        if connection.is_connected():
-            cursor = connection.cursor()
-
-            # CREATE DATABASE (NO SELECT / SHOW USED)
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-
-            print("Database 'alx_book_store' created successfully!")
-   except mysql.connector.Error as e:
-       print(e)
 
    finally:
-        # CLOSE CONNECTION
-        if 'connection' in locals() and connection.is_connected():
-            cursor.close()
-            connection.close()
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
 
 if __name__ == "__main__":
     create_database()
